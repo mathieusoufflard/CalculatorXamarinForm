a4ee01c7c027e8128cb45ba368e75e699abc70cc
PC-ASUS-ROG <mathieusoufflard@gmail.com>
2020-05-29 16:19:22 +0200

    Update applicationhost.config, .suo, and 3452 more files...

---
+++ calculator/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "4ab40168-bcc6-443e-bd22-6bb4f5a85eb3",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
